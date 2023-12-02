1ce6ff32a128b1aa60298882cba39d6bea9bd871
ajithshine <ajithshine77@gmail.com>
2023-12-02 22:24:11 +0530

    initial

---
+++ index.js
@@ -0,0 +1,34 @@
+const container = document.getElementById('container');
+let currentPosition = 0;
+
+imageMapper = [
+    
+]
+
+// Retrieve the last question from localStorage
+const lastQuestion = localStorage.getItem('lastQuestion');
+if (lastQuestion) {
+    showQuestion(parseInt(lastQuestion));
+}
+
+function showQuestion(questionNumber) {
+    currentPosition = -(questionNumber - 1) * 100;
+    container.style.transform = `translateX(${currentPosition}vw)`;
+
+    // Save the current question to localStorage
+    localStorage.setItem('lastQuestion', questionNumber);
+}
+
+function showImage(label) {
+    window.location.href = label;
+}
+
+// Handle arrow key presses
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowLeft') {
+    showQuestion(currentPosition / -100);
+    } else if (event.key === 'ArrowRight') {
+    showQuestion(currentPosition / -100 + 2);
+    }
+});
+
