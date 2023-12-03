48c4ef8354a0f107953de602b219cdf4620906f9
ajithshine <ajithshine77@gmail.com>
2023-12-04 01:57:26 +0530

    wireframe accordion

--- imageScript.js
+++ imageScript.js
@@ -19,3 +19,21 @@ window.onload = function() {
       imageId.src = imagePath;
     }
 }
+
+function toggleAccordion(sectionId) {
+  var content = document.getElementById(sectionId);
+  if (content.style.display === 'block') {
+    content.style.display = 'none';
+  } else {
+    content.style.display = 'block';
+  }
+}
+
+function toggleAccordionContent(sectionId) {
+  var content = document.getElementById(sectionId);
+  if (content.style.display === 'block') {
+    content.style.display = 'none';
+  } else {
+    content.style.display = 'block';
+  }
+}
